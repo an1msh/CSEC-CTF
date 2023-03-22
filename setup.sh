d31b56c982d1c1363d6e970d4749eb28f7c7edc8
Riyush Ghimire <48800246+richighimi@users.noreply.github.com>
2023-03-22 08:08:44 -0400

    deploy on port 80

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo NEEDRESTART_MODE=a apt-get install -y snapd
 sudo snap install docker
 sudo docker build -t csecctf .
-sudo docker run -t -p 96:96 csecctf
+sudo docker run -t -p 80:80 csecctf
